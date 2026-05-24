f04f17ff3287f273b394aaf48d0f0b7d5ee27db8
Sukuna092main <baohungvonguyen2005@gmail.com>
2026-05-24 22:08:11 +0700

    feat(admin): tạo admin

--- backend/src/routes/menuRoutes.js
+++ backend/src/routes/menuRoutes.js
@@ -1,9 +1,24 @@
 const express = require('express')
 const router = express.Router();
+const authMiddleware = require('../middleware/authMiddleware')
+const {requiredRole} = require('../middleware/roleMiddleware')
 
 const authMiddleware = require('../middleware/authMiddleware')
-const { getMenu } = require('../controllers/menuController')
+const { getMenu,
+    getTodayMenu,
+    getMenuItem,
+    createMenuItem,
+    updateMenuItem,
+    deleteMenuItem,
+    setTodayMenu
+ } = require('../controllers/menuController')
 
 router.get('/', authMiddleware, getMenu);
+router.get('/today', authMiddleware, getTodayMenu);
+router.get('/:id', authMiddleware, getMenuItem);
+router.post('/', authMiddleware, requiredRole('admin'), createMenuItem);
+router.put('/:id', authMiddleware, requiredRole('admin'), updateMenuItem);
+router.delete('/:id', authMiddleware, requiredRole('admin'), deleteMenuItem);
+router.patch('/today', authMiddleware, requiredRole('staff','admin'), setTodayMenu);
 
 module.exports = router;
